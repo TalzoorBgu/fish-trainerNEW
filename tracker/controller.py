535266b583d327bd5288bfcf5b5e6891c37e0108
Tal <zoor@post.bgu.ac.il>
2018-08-28 10:32:10 +0300

    "plotter implementing"

--- tracker/controller.py
+++ tracker/controller.py
@@ -36,9 +36,15 @@ class Controller:
         width = track_fish.init_tracking()
 
         # init logger
-        full_script_path = '{}{}'.format(os.path.dirname(os.path.realpath(__file__)), '/')
+        #   full_script_path = '{}{}'.format(os.path.dirname(os.path.realpath(__file__)), '/')
+        #   full_root_script_path = full_script_path[:full_script_path.find('tracker')]
+        #   log_folder = '{}data/log/'.format(full_root_script_path)
+        full_script_path = os.path.dirname(os.path.realpath(__file__))
         full_root_script_path = full_script_path[:full_script_path.find('tracker')]
         log_folder = '{}data/log/'.format(full_root_script_path)
+        print("full_script_path:{}\nfull_root_script_path:{}\nlog_folder:{}".
+              format(full_script_path, full_root_script_path, log_folder))
+
         print('log:{}'.format(log_folder))
         #print_and_update_main_log('log:{}'.format(log_folder))
         self.logger = []
