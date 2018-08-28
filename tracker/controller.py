06b0065f615559a8ba12d39ea4115c3a32d398e3
Tal <zoor@post.bgu.ac.il>
2018-08-28 10:50:12 +0300

    "plotter implementing"

--- tracker/controller.py
+++ tracker/controller.py
@@ -41,7 +41,7 @@ class Controller:
         #   log_folder = '{}data/log/'.format(full_root_script_path)
         full_script_path = os.path.dirname(os.path.realpath(__file__))
         full_root_script_path = full_script_path[:full_script_path.find(r'\tracker')]
-        log_folder = os.path.join(full_root_script_path, r"data\log\"")
+        log_folder = os.path.join(full_root_script_path, r"data\log", r"\"")
         print("full_script_path:{}\nfull_root_script_path:{}\nlog_folder:{}".
               format(full_script_path, full_root_script_path, log_folder))
 
